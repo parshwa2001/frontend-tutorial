98d2a929f7b72a13d6f8710ba3c35d8b7d7aedf4
parshwa999 <parshwa@eryushion.com>
2023-11-28 18:04:05 +0530

    update

--- src/index.js
+++ src/index.js
@@ -3,6 +3,23 @@ import { createRoot } from "react-dom/client";
 import App from "./components/App";
 
 // Import and init Sentry SDK
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+    dsn: "https://e8b633028129a01298983cbce72baf33@o4506302658904064.ingest.sentry.io/4506303412043776",
+    integrations: [
+      new Sentry.BrowserTracing({
+        // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+        tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+      }),
+      new Sentry.Replay(),
+    ],
+    // Performance Monitoring
+    tracesSampleRate: 1.0, // Capture 100% of the transactions
+    // Session Replay
+    replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+  });
 
 const container = document.getElementById("root");
 const root = createRoot(container);
